ec05c4868a88a223e2728a4a5ee1c0137b9b54e2
tom <tomhall4321@gmail.com>
2020-11-06 11:19:41 +0000

    complete library

--- app/Library/Library.php
+++ app/Library/Library.php
@@ -5,5 +5,28 @@ namespace App\Library;
 
 class Library
 {
-    
+    private $shelves;
+
+    public function __construct()
+    {
+        $this->shelves = collect();
+    }
+
+    public function addShelf(Shelf $shelf) : Library
+    {
+        $this->shelves->push($shelf);
+        return $this;
+    }
+
+    public function titles() : array
+    {
+        $arrayOfTitleArrays = $this->shelves->map(fn($shelf)=> $shelf->titles());
+        $oneArray = [];
+        foreach($arrayOfTitleArrays as $shelf) 
+        {
+        $oneArray = array_merge($output, $shelf);
+        }
+        return $oneArray;
+
+    }
 }
